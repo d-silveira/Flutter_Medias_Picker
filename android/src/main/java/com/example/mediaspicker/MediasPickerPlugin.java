fae6800241299c81aca8e377816c2a5060fc2454
Duarte Silveira <duarte.silveira@streambow.com>
2018-07-17 16:13:37 +0100

    make quantity optional

--- android/src/main/java/com/example/mediaspicker/MediasPickerPlugin.java
+++ android/src/main/java/com/example/mediaspicker/MediasPickerPlugin.java
@@ -1,12 +1,5 @@
 package com.example.mediaspicker;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.UUID;
-
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
@@ -21,13 +14,19 @@ import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.UUID;
+
 import droidninja.filepicker.FilePickerBuilder;
 import droidninja.filepicker.FilePickerConst;
-
+import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
@@ -35,287 +34,299 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
  * MediasPickerPlugin
  */
 public class MediasPickerPlugin implements MethodCallHandler, PluginRegistry.ActivityResultListener, PluginRegistry.RequestPermissionsResultListener {
-  /**
-   * Plugin registration.
-   */
+	/**
+	 * Plugin registration.
+	 */
 
 
-  private Activity activity;
-  private Result result;
-  private int maxWidth, maxHeight, quality;
-  private boolean isPhoto;
+	private Activity activity;
+	private Result   result;
+	private int      maxWidth, maxHeight, quality;
+	private boolean isPhoto;
 
-  private MediasPickerPlugin(Activity activity) {
-    this.activity = activity;
-  }
+	private MediasPickerPlugin(Activity activity) {
+		this.activity = activity;
+	}
 
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "medias_picker");
-    MediasPickerPlugin plugin = new MediasPickerPlugin(registrar.activity());
-    channel.setMethodCallHandler(plugin);
-    registrar.addActivityResultListener(plugin);
-  }
-
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-
-    if (call.method.equals("pickImages")) {
-
-      isPhoto = true;
-
-      int quantity = call.argument("quantity");
-      maxWidth = call.argument("maxWidth");
-      maxHeight = call.argument("maxHeight");
-      quality = call.argument("quality");
-
-      this.result = result;
-      FilePickerBuilder.getInstance().setMaxCount(quantity)
-              .enableVideoPicker(false)
-              .enableImagePicker(true)
-              .pickPhoto(activity);
+	public static void registerWith(Registrar registrar) {
+		final MethodChannel channel = new MethodChannel(registrar.messenger(), "medias_picker");
+		MediasPickerPlugin plugin = new MediasPickerPlugin(registrar.activity());
+		channel.setMethodCallHandler(plugin);
+		registrar.addActivityResultListener(plugin);
+	}
 
 
-    } else if (call.method.equals("pickVideos")) {
+	@Override
+	public void onMethodCall(MethodCall call, Result result) {
 
-      isPhoto = false;
+		if (call.method.equals("pickImages")) {
 
-      int quantity = call.argument("quantity");
+			isPhoto = true;
 
-      this.result = result;
-      FilePickerBuilder.getInstance().setMaxCount(quantity)
-              .enableVideoPicker(true)
-              .enableImagePicker(false)
-              .pickPhoto(activity);
+			int quantity = 0;
+			if (call.hasArgument("quantity")) {
+				quantity = call.argument("quantity");
+			}
+			maxWidth = call.argument("maxWidth");
+			maxHeight = call.argument("maxHeight");
+			quality = call.argument("quality");
 
-    } else if (call.method.equals("deleteAllTempFiles")) {
-      this.result = result;
-      DeleteAllTempFiles();
-    } else if (call.method.equals("compressImages")) {
-      maxWidth = call.argument("maxWidth");
-      maxHeight = call.argument("maxHeight");
-      quality = call.argument("quality");
-      ArrayList<String> imgPaths = call.argument("imgPaths");
-      ArrayList<String> newImgPaths = new ArrayList<>();
+			this.result = result;
+			FilePickerBuilder filePickerBuilder = FilePickerBuilder.getInstance();
+			if (quantity > 0) {
+				filePickerBuilder = filePickerBuilder.setMaxCount(quantity);
+			}
+			filePickerBuilder.enableVideoPicker(false)
+							 .enableImagePicker(true)
+							 .pickPhoto(activity);
 
-      for (String path : imgPaths) {
-        String newPath = CompressImage(path, maxWidth, maxHeight, quality);
+		} else if (call.method.equals("pickVideos")) {
 
-        if (newPath != null && newPath != "")
-          newImgPaths.add(newPath);
-          
-      }
-      this.result = result;
-      this.result.success(newImgPaths);
+			isPhoto = false;
 
-    } else if (call.method.equals("checkPermission")) {
-        result.success(checkPermission());
-    } else if (call.method.equals("requestPermission")) {
-        this.result = result;
-        requestPermission();
-    } else {
-      result.notImplemented();
-    }
-  }
+			int quantity = 0;
+			if (call.hasArgument("quantity")) {
+				quantity = call.argument("quantity");
+			}
 
-  public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
-    final int height = options.outHeight;
-    final int width = options.outWidth;
-    int inSampleSize = 1;
+			this.result = result;
+			FilePickerBuilder filePickerBuilder = FilePickerBuilder.getInstance();
+			if (quantity > 0) {
+				filePickerBuilder = filePickerBuilder.setMaxCount(quantity);
+			}
+			filePickerBuilder.enableVideoPicker(true)
+							 .enableImagePicker(false)
+							 .pickPhoto(activity);
 
-    if (height > reqHeight || width > reqWidth) {
-      final int heightRatio = Math.round((float) height / (float) reqHeight);
-      final int widthRatio = Math.round((float) width / (float) reqWidth);
-      inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
-    }
-    final float totalPixels = width * height;
-    final float totalReqPixelsCap = reqWidth * reqHeight * 2;
-    while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
-      inSampleSize++;
-    }
-    return inSampleSize;
-  }
+		} else if (call.method.equals("deleteAllTempFiles")) {
+			this.result = result;
+			DeleteAllTempFiles();
+		} else if (call.method.equals("compressImages")) {
+			maxWidth = call.argument("maxWidth");
+			maxHeight = call.argument("maxHeight");
+			quality = call.argument("quality");
+			ArrayList<String> imgPaths = call.argument("imgPaths");
+			ArrayList<String> newImgPaths = new ArrayList<>();
 
+			for (String path : imgPaths) {
+				String newPath = CompressImage(path, maxWidth, maxHeight, quality);
 
-    public String CompressImage(String filename, int maxWidth, int maxHeight, int quality) {
+				if (newPath != null && newPath != "") {
+					newImgPaths.add(newPath);
+				}
 
-        Bitmap scaledBitmap = null;
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        Bitmap bmp = BitmapFactory.decodeFile(filename, options);
+			}
+			this.result = result;
+			this.result.success(newImgPaths);
 
-        int actualHeight = options.outHeight;
-        int actualWidth = options.outWidth;
+		} else if (call.method.equals("checkPermission")) {
+			result.success(checkPermission());
+		} else if (call.method.equals("requestPermission")) {
+			this.result = result;
+			requestPermission();
+		} else {
+			result.notImplemented();
+		}
+	}
 
-        float imgRatio = (float) actualWidth / (float) actualHeight;
-        float maxRatio = (float)maxWidth / (float)maxHeight;
+	public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
 
-        if (actualHeight > maxHeight || actualWidth > maxWidth) {
-            if (imgRatio < maxRatio) {
-                imgRatio = (float)maxHeight / actualHeight;
-                actualWidth = (int) (imgRatio * actualWidth);
-                actualHeight = maxHeight;
-            } else if (imgRatio > maxRatio) {
-                imgRatio = (float)maxWidth / actualWidth;
-                actualHeight = (int) (imgRatio * actualHeight);
-                actualWidth = maxWidth;
-            }
-        }
-
-        options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight);
-        options.inJustDecodeBounds = false;
-        options.inDither = false;
-        options.inPurgeable = true;
-        options.inInputShareable = true;
-        options.inTempStorage = new byte[16 * 1024];
-
-        try {
-            bmp = BitmapFactory.decodeFile(filename, options);
-        } catch (OutOfMemoryError exception) {
-            exception.printStackTrace();
-        }
-        try {
-            scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.RGB_565);
-        } catch (OutOfMemoryError exception) {
-            exception.printStackTrace();
-        }
-
-        float ratioX = actualWidth / (float) options.outWidth;
-        float ratioY = actualHeight / (float) options.outHeight;
-        float middleX = actualWidth / 2.0f;
-        float middleY = actualHeight / 2.0f;
-
-        Matrix scaleMatrix = new Matrix();
-        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
-        Canvas canvas = new Canvas(scaledBitmap);
-        canvas.setMatrix(scaleMatrix);
-        canvas.drawBitmap(bmp, middleX - bmp.getWidth() / 2, middleY - bmp.getHeight() / 2, new Paint(Paint.FILTER_BITMAP_FLAG));
-
-        if (bmp != null) {
-            bmp.recycle();
-        }
-
-        ExifInterface exif;
-
-        try {
-            exif = new ExifInterface(filename);
-            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0);
-            Matrix matrix = new Matrix();
-            if (orientation == 6) {
-                matrix.postRotate(90);
-            } else if (orientation == 3) {
-                matrix.postRotate(180);
-            } else if (orientation == 8) {
-                matrix.postRotate(270);
-            }
-            scaledBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        // compress to the format you want, JPEG, PNG...
-        // 70 is the 0-100 quality percentage
-        scaledBitmap.compress(Bitmap.CompressFormat.JPEG, quality , outStream);
-
-        String tempDirPath = Environment.getExternalStorageDirectory()
-                + File.separator + "TempImgs" + File.separator;
-        String path = tempDirPath + UUID.randomUUID().toString() + ".jpg";
-
-        File tempDir = new File(tempDirPath);
-
-        File f = new File(path);
-        try {
-            if (!tempDir.exists())
-                tempDir.mkdirs();
-
-            f.createNewFile();
-
-            //write the bytes in file
-            FileOutputStream fo = new FileOutputStream(f);
-            fo.write(outStream.toByteArray());
-            // remember close de FileOutput
-            fo.close();
-
-            return path;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return filename;
-    }
+		if (height > reqHeight || width > reqWidth) {
+			final int heightRatio = Math.round((float) height / (float) reqHeight);
+			final int widthRatio = Math.round((float) width / (float) reqWidth);
+			inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+		}
+		final float totalPixels = width * height;
+		final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+		while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+			inSampleSize++;
+		}
+		return inSampleSize;
+	}
 
 
-  public void DeleteAllTempFiles(){
-    String tempDirPath = Environment.getExternalStorageDirectory()
-            + File.separator + "TempImgs" + File.separator;
+	public String CompressImage(String filename, int maxWidth, int maxHeight, int quality) {
 
-    File tempDir = new File(tempDirPath);
-    if (tempDir.exists()){
+		Bitmap scaledBitmap = null;
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		Bitmap bmp = BitmapFactory.decodeFile(filename, options);
 
-      String[] children = tempDir.list();
-      for (int i = 0; i < children.length; i++)
-      {
-        new File(tempDir, children[i]).delete();
-      }
+		int actualHeight = options.outHeight;
+		int actualWidth = options.outWidth;
 
-      if (tempDir.delete())
-        this.result.success(true);
-      else
-        this.result.success(false);
-    } else {
-      this.result.success(true);
-    }
-  }
+		float imgRatio = (float) actualWidth / (float) actualHeight;
+		float maxRatio = (float) maxWidth / (float) maxHeight;
 
-    private void requestPermission() {
-        String[] perm = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
-        ActivityCompat.requestPermissions(activity, perm, 0);
-    }
+		if (actualHeight > maxHeight || actualWidth > maxWidth) {
+			if (imgRatio < maxRatio) {
+				imgRatio = (float) maxHeight / actualHeight;
+				actualWidth = (int) (imgRatio * actualWidth);
+				actualHeight = maxHeight;
+			} else if (imgRatio > maxRatio) {
+				imgRatio = (float) maxWidth / actualWidth;
+				actualHeight = (int) (imgRatio * actualHeight);
+				actualWidth = maxWidth;
+			}
+		}
 
-    private boolean checkPermission() {
-        return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-    }
+		options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight);
+		options.inJustDecodeBounds = false;
+		options.inDither = false;
+		options.inPurgeable = true;
+		options.inInputShareable = true;
+		options.inTempStorage = new byte[16 * 1024];
 
-    @Override
-    public boolean onRequestPermissionsResult(int requestCode, String[] strings, int[] grantResults) {
-        boolean res = false;
-        if (requestCode == 0 && grantResults.length > 0) {
-            res = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-            result.success(res);
-        }
-        return res;
-    }
+		try {
+			bmp = BitmapFactory.decodeFile(filename, options);
+		} catch (OutOfMemoryError exception) {
+			exception.printStackTrace();
+		}
+		try {
+			scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.RGB_565);
+		} catch (OutOfMemoryError exception) {
+			exception.printStackTrace();
+		}
 
-  @Override
-  public boolean onActivityResult(int requestCode, int resultCode, Intent intent) {
+		float ratioX = actualWidth / (float) options.outWidth;
+		float ratioY = actualHeight / (float) options.outHeight;
+		float middleX = actualWidth / 2.0f;
+		float middleY = actualHeight / 2.0f;
 
-    if (requestCode == FilePickerConst.REQUEST_CODE_PHOTO){
+		Matrix scaleMatrix = new Matrix();
+		scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
+		Canvas canvas = new Canvas(scaledBitmap);
+		canvas.setMatrix(scaleMatrix);
+		canvas.drawBitmap(bmp, middleX - bmp.getWidth() / 2, middleY - bmp.getHeight() / 2, new Paint(Paint.FILTER_BITMAP_FLAG));
 
-      ArrayList<String> docPaths = new ArrayList<>();
+		if (bmp != null) {
+			bmp.recycle();
+		}
 
-      if (intent != null){
-        ArrayList<String> paths = intent.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_MEDIA);
+		ExifInterface exif;
 
-        if (isPhoto) {
-          for(String item: paths){
+		try {
+			exif = new ExifInterface(filename);
+			int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0);
+			Matrix matrix = new Matrix();
+			if (orientation == 6) {
+				matrix.postRotate(90);
+			} else if (orientation == 3) {
+				matrix.postRotate(180);
+			} else if (orientation == 8) {
+				matrix.postRotate(270);
+			}
+			scaledBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-            String path = CompressImage(item, maxWidth, maxHeight , quality);
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		// compress to the format you want, JPEG, PNG...
+		// 70 is the 0-100 quality percentage
+		scaledBitmap.compress(Bitmap.CompressFormat.JPEG, quality, outStream);
 
-            if (path != null)
-              docPaths.add(path);
-          }
-        } else {
-          docPaths = paths;
-        }
+		String tempDirPath = Environment.getExternalStorageDirectory() + File.separator + "TempImgs" + File.separator;
+		String path = tempDirPath + UUID.randomUUID().toString() + ".jpg";
 
-      }
-    
-      this.result.success(docPaths);
-    
-      return true;
-    }
+		File tempDir = new File(tempDirPath);
 
-    return false;
-  }
+		File f = new File(path);
+		try {
+			if (!tempDir.exists()) {
+				tempDir.mkdirs();
+			}
+
+			f.createNewFile();
+
+			//write the bytes in file
+			FileOutputStream fo = new FileOutputStream(f);
+			fo.write(outStream.toByteArray());
+			// remember close de FileOutput
+			fo.close();
+
+			return path;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return filename;
+	}
+
+
+	public void DeleteAllTempFiles() {
+		String tempDirPath = Environment.getExternalStorageDirectory() + File.separator + "TempImgs" + File.separator;
+
+		File tempDir = new File(tempDirPath);
+		if (tempDir.exists()) {
+
+			String[] children = tempDir.list();
+			for (int i = 0; i < children.length; i++) {
+				new File(tempDir, children[i]).delete();
+			}
+
+			if (tempDir.delete()) {
+				this.result.success(true);
+			} else {
+				this.result.success(false);
+			}
+		} else {
+			this.result.success(true);
+		}
+	}
+
+	private void requestPermission() {
+		String[] perm = { Manifest.permission.WRITE_EXTERNAL_STORAGE };
+		ActivityCompat.requestPermissions(activity, perm, 0);
+	}
+
+	private boolean checkPermission() {
+		return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+	}
+
+	@Override
+	public boolean onRequestPermissionsResult(int requestCode, String[] strings, int[] grantResults) {
+		boolean res = false;
+		if (requestCode == 0 && grantResults.length > 0) {
+			res = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+			result.success(res);
+		}
+		return res;
+	}
+
+	@Override
+	public boolean onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+		if (requestCode == FilePickerConst.REQUEST_CODE_PHOTO) {
+
+			ArrayList<String> docPaths = new ArrayList<>();
+
+			if (intent != null) {
+				ArrayList<String> paths = intent.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_MEDIA);
+
+				if (isPhoto) {
+					for (String item : paths) {
+
+						String path = CompressImage(item, maxWidth, maxHeight, quality);
+
+						if (path != null) {
+							docPaths.add(path);
+						}
+					}
+				} else {
+					docPaths = paths;
+				}
+
+			}
+
+			this.result.success(docPaths);
+
+			return true;
+		}
+
+		return false;
+	}
 }
